5249c5cadf6d3ec9e7ae0f73059f516ef71ebdfb
ahmedh12 <ahmedhussien783@gmail.com>
2023-04-29 22:01:58 +0300

    update dataset and collate function to interface with the evaluation function

--- models/utils/datasets.py
+++ models/utils/datasets.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import time
+import numpy as np
 
 # Ignore warnings
 import warnings
@@ -34,28 +35,35 @@ class RTTS_Dataset(Dataset):
         return len(self.img_names)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir,
+        img_path = os.path.join(self.root_dir,
                                 self.img_names[idx])
-        label_name = os.path.join(self.label_dir,
+        label_path = os.path.join(self.label_dir,
                                 self.img_names[idx].replace('.png', '.txt'))
-        image = cv2.imread(img_name)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        label = open(label_name, 'r').readlines()
-        label = [l.strip().split(' ') for l in label]
-        label = [[int(l[0]), float(l[1]), float(l[2]), float(l[3]), float(l[4])] for l in label]
+        image = cv2.imread(img_path)
+        image = [image]
+        shape =  [image[0].shape[0], image[0].shape[1]]
+        labels = open(label_path, 'r').readlines()
+        labels = [l.strip().split(' ') for l in labels]
+        labels = [[int(l[0]), float(l[1]), float(l[2]), float(l[3]), float(l[4])] for l in labels]
+        #list of lists to numpy array
+        labels = np.array(labels)
+        nL = len(labels)  # number of labels
+        labels_out = torch.zeros((nL, 6))
+        if nL:
+            labels_out[:, 1:] = torch.from_numpy(labels)
 
         if self.transform:
-            image = self.transform(image)
-        return image , label , img_name , (image.shape[1], image.shape[2])
+            image = self.transform(image[0])
+        return image , labels_out , img_path , shape
     
+
     def collate_fn(self, batch):
-        images = []
-        labels = []
-        for b in batch:
-            images.append(b[0])
-            labels.append(b[1])
-        images = torch.stack(images, dim=0) # (B, C, H, W)
-        return images, labels
+        images , labels , paths , shapes =  zip(*batch)
+        images = torch.cat(images, dim=0)
+        for i , l in enumerate(labels):
+            l[: , 0] = i
+        labels = torch.cat(labels, dim=0)
+        return images, labels , paths , shapes
     
 class Resize(object):
     """Resize ndarrays in sample to Tensors."""
@@ -70,8 +78,12 @@ class Resize(object):
 class ToTensor(object):
     """Convert ndarrays in sample to Tensors. (H,W,C) -> (C,H,W))"""
 
-    def __call__(self, image):
-        return torch.from_numpy(image).permute(2,0,1).float() / 255.0
+    def __call__(self, img):
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, HWC to CHW
+        img = np.ascontiguousarray(img)  # ascontiguousarray
+        img = torch.from_numpy(img).unsqueeze(0)
+        return img.float().div(255.0)
+        # return torch.from_numpy(image).permute(2,0,1).float() / 255.0
 
 #############################################################Test Functions############################################################################
 def test_RTTS_Dataset():
@@ -81,18 +93,19 @@ def test_RTTS_Dataset():
     start = time.time()
     #show the first 4 images in a 2*2 grid
     for i in range(len(rtts_dataset)):
-        image , label = rtts_dataset[i]
-        image = image.permute(1,2,0)
+        image , labels , _ , _ = rtts_dataset[i]
         print(i, image.shape)
+        image = image[0].permute(1,2,0)
         ax = plt.subplot(2, 2, i + 1)
         ax.set_title('Sample #{}'.format(i))
         ax.axis('off')
         #show bounding boxes
-        for l in label:
-            x1 = (l[1] - l[3]/2) * image.shape[1]
-            y1 = (l[2] - l[4]/2) * image.shape[0]
-            x2 = (l[1] + l[3]/2) * image.shape[1]
-            y2 = (l[2] + l[4]/2) * image.shape[0]
+
+        for l in labels: # l = [0 , class , x , y , w , h]
+            x1 = (l[2] - l[4]/2) * image.shape[1]
+            y1 = (l[3] - l[5]/2) * image.shape[0]
+            x2 = (l[2] + l[4]/2) * image.shape[1]
+            y2 = (l[3] + l[5]/2) * image.shape[0]
             rect = plt.Rectangle((x1, y1), x2-x1, y2-y1, fill=False, edgecolor='g', linewidth=1)
             ax.add_patch(rect)
 
@@ -103,30 +116,32 @@ def test_RTTS_Dataset():
             break
 
 
-def test_RTTS_Dataloader():
+def test_RTTS_Dataloader(batch_size=4):
     #create a dataset object
     rtts_dataset = RTTS_Dataset(root_dir='../datasets/RTTS/images/val/' , transform=transforms.Compose([Resize(544), ToTensor()]))
     #create a dataloader object
-    dataloader = DataLoader(rtts_dataset, batch_size=4, shuffle=True, num_workers = 0 , collate_fn=rtts_dataset.collate_fn)
+    dataloader = DataLoader(rtts_dataset, batch_size=batch_size, shuffle=True, num_workers = 0 , collate_fn=rtts_dataset.collate_fn)
     #record time
     start = time.time()
     #show the first 4 batches of 4 images each batch in a  2*2 grid , and all batches in a 2*2 grid with bounding boxes using grid of subplots
     _ , axs = plt.subplots(nrows=4 , ncols=4 , figsize=(12,12))
-    for i_batch, (images , labels) in enumerate(dataloader):
-        for j in range(images.shape[0]):
-
+    for i_batch, (images , labels , img_paths , sizes) in enumerate(dataloader):
+        
+        for j in range(batch_size):
             #show images
             ax = axs[i_batch][j]
             ax.set_title('Batch #{} , Sample #{}'.format(i_batch , j))
             ax.axis('off')
-            ax.imshow(images[j].permute(1,2,0))
+            #get the image as the firsyt 3 channels of the tensor
+            curr_image = images[j]
+            ax.imshow(curr_image.permute(1,2,0))
 
             #show bounding boxes
-            for l in labels[j]:
-                x1 = (l[1] - l[3]/2) * images[j].shape[1]
-                y1 = (l[2] - l[4]/2) * images[j].shape[2]
-                x2 = (l[1] + l[3]/2) * images[j].shape[1]
-                y2 = (l[2] + l[4]/2) * images[j].shape[2]
+            for l in labels[labels[:,0] == j][: , 1:]: # l = [class , x , y , w , h]
+                x1 = (l[1] - l[3]/2) * curr_image.shape[1]
+                y1 = (l[2] - l[4]/2) * curr_image.shape[2]
+                x2 = (l[1] + l[3]/2) * curr_image.shape[1]
+                y2 = (l[2] + l[4]/2) * curr_image.shape[2]
                 rect = plt.Rectangle((x1, y1), x2-x1, y2-y1, fill=False, edgecolor='g', linewidth=1)
                 ax.add_patch(rect)
             
